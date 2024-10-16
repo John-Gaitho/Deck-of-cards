152de759701b7c9440d855d17d0eb7d6eb6659b7
John Gaitho <jgaitho016@gmail.com>
2024-10-16 20:44:32 +0300

    eventlisteners

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ fetch(deckUrl)
   .then(data => console.log(data));
   
 
-  const deck_id = "8fdxs9viczhm"
+const deck_id = "8fdxs9viczhm"
   fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
   .then(response => response.json())
   .then(data => console.log(data));  
   
 
+let btn = document.getElementById('button');
+btn.addEventListener('click', function() {
+
+
+
+}
 
 });
