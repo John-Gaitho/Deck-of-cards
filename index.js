c1ae3acef141df3f3fd02bab2c67f90230b13830
John Gaitho <jgaitho016@gmail.com>
2024-10-16 17:20:18 +0300

    draw card

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ fetch(deckUrl)
   
   .then(data => console.log(data));
   
+
+  const deck_id = "8fdxs9viczhm"
+  fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
+  .then(response => response.json())
+  .then(data => console.log(data));  
+  let deckid = deck_id; 
