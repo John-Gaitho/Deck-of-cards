e82cfc18770ec70647f7712b0c21e974e31422e8
John Gaitho <jgaitho016@gmail.com>
2024-10-17 17:55:22 +0300

    change to async

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 
+async function createDeck() {
+  const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+  const deckDetails = await response.json();
+  console.log(deckDetails);
+}
+createDeck();
 
-const deck_id = "luvcg5u95nk7";
-const deckUrl =  'https://deckofcardsapi.com/api/deck/new/shuffle/';
-const cardsContainer = document.getElementById("cards");
-let image = "https://deckofcardsapi.com/static/img/QS.svg"
-let card = "https://deckofcardsapi.com/static/img/QS.png"
+/*const deckUrl =  'https://deckofcardsapi.com/api/deck/new/shuffle/';
+const cardsContainer = document.getElementById("card");
 document.addEventListener("DOMContentLoaded",() => {
-
 fetch(deckUrl)
-  .then(response => response.json())
-  
+  .then(res => res.json())
+    
+
+    
+});
+
 fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
-    .then(response => response.json())
+    .then(res => res.json())
       
     
 let btn = document.getElementById('button');
@@ -20,22 +26,25 @@ btn.addEventListener('click', function() {
 const count = 1; // number of cards to draw
 fetch(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${count}`)
     .then(res => res.json())
-    .then(data => console.log(data));
-        
-  
-    cardsContainer.innerText += image
-      
-      
+    //.then(card => console.log(cards)
+    const cardsDetails = card.cards;
+cardsContainer.innerText +=`<img id="card" src="${cardsDetails}" />`
 
 }); 
   
 
-Form.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', (e) => {
+  e.preventDefauit()
+
+  var answer = document.getElementById('age').value
+  console.log(answer)
   let messages = []
   if(submit.value === 'No') {
     messages.push('Sorry, you cant play!')
   } else ('Welcome player')
-  e.preventDefault()
+ 
 });
 
 });
+*/
